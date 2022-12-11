b7b2cf38cb507a978b284de754710ae9f51ccbe1
ksuniry <ksuniry@DESKTOP-T8CHSSE>
2022-12-11 22:10:43 +0900

    test

--- src/test/java/com/example/lt/Lt220629ApplicationTests.java
+++ src/test/java/com/example/lt/Lt220629ApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.lt;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test
 @SpringBootTest
 class Lt220629ApplicationTests {
 
